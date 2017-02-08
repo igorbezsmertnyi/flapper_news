be41aa2e44223abe027a7533dad83175a6710329
Igor <mr.igor96@outlook.com>
2017-02-08 22:57:38 +0100

    added function to comment and upvote posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-   params.require(:comment).permit(:body)
+   params.require(:comment).permit(:body, :author)
   end
 
 end
